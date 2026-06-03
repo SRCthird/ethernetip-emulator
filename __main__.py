2fca54be84e91b9b405c954d46a1782f0593f4ef
Stephen Chryn <SRCthird@gmail.com>
2026-06-03 12:58:09 -0400

    Update simple strings to regular strings

--- __main__.py
+++ __main__.py
@@ -8,16 +8,16 @@ from src.server.tag_specs import TAG_ARGV
 
 def I_TEXT(attr, key, value) -> None:
     registry = getattr(type(attr), "registry", {})
-    output_tag = registry.get("O_TEXT")
+    output_tag = registry.get("O_TEXT.DATA")
     output_count_done = registry.get("O_Updates.DN")
     if output_tag is not None:
         try:
             AttributeDevice._actions.counter.increment("O_Updates")
             AttributeDevice._actions.increment.increment("O_Updates.LAZY")
             if output_count_done is not None and output_count_done[key][0]:
-                output_tag[key] = ["Count Completed"]
+                AttributeDevice._actions.string.set_val("O_TEXT", key, "Count Completed")
             else:
-                output_tag[key] = value
+                AttributeDevice._actions.string.set_val("O_TEXT", key, value)
         except Exception:
             if hasattr(output_tag, "value"):
                 setattr(output_tag, "value", value)
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         actions.increment.start(tag_name="O_INCR", wrap=32766)
         actions.timer.start("O_Timer", enable="O_Timer.EN")
         actions.on_change("O_Timer.DN", on_timer_done)
-        actions.on_change("I_TEXT", I_TEXT)
+        actions.on_change("I_TEXT.DATA", I_TEXT)
         actions.on_change("O_Updates.RES", reset_timer)
     
         enip_main(
