0d17b9f186546cf7cb00d21f0db99d9b542145ce
Stephen Chryn <SRCthird@gmail.com>
2026-07-07 16:17:35 -0400

    Update location comment pt2

--- src/server/actions.py
+++ src/server/actions.py
@@ -1,7 +1,7 @@
 # Copyright 2026 Merck KGaA, Darmstadt, Germany and/or its affiliates.
 # All rights reserved
 
-# src/server/actions/actions.py
+# src/server/actions.py
 from __future__ import annotations
 
 import threading
