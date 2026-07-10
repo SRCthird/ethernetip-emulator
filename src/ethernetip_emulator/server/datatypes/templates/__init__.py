fa889b52bb1ab86e8e62ad6e341cc1c6b27dc7f0
Stephen Chryn <SRCthird@gmail.com>
2026-07-10 13:25:21 -0400

    Import templates

--- src/ethernetip_emulator/server/datatypes/templates/__init__.py
+++ src/ethernetip_emulator/server/datatypes/templates/__init__.py
@@ -1,4 +1,7 @@
 from src.ethernetip_emulator.server.datatypes.templates.basic import Basic
 from src.ethernetip_emulator.server.datatypes.templates.boolarray import BoolArray
+from src.ethernetip_emulator.server.datatypes.templates.numericarray import NumericArray
+from src.ethernetip_emulator.server.datatypes.templates.realarray import RealArray
+from src.ethernetip_emulator.server.datatypes.templates.stringarray import StringArray
 
-__all__ = ["Basic", "BoolArray"]
+__all__ = ["Basic", "BoolArray", "NumericArray", "RealArray", "StringArray"]
