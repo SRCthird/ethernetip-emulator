511ef2dab6371349347ed0cec96dcc7d7b60b7fd
Stephen Chryn <SRCthird@gmail.com>
2026-07-10 13:25:15 -0400

    Update key usage in basic template

--- src/ethernetip_emulator/server/datatypes/templates/basic.py
+++ src/ethernetip_emulator/server/datatypes/templates/basic.py
@@ -1,9 +1,9 @@
 # Copyright 2026 Merck KGaA, Darmstadt, Germany and/or its affiliates.
 # All rights reserved
 
-# src/server/datatypes/templates/basic.py
+# src/ethernetip_emulator/server/datatypes/templates/basic.py
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, List
 
 if TYPE_CHECKING:
     from src.ethernetip_emulator.server.actions import AttributeActions
@@ -12,13 +12,13 @@ class Basic:
     def __init__(self, parent: AttributeActions):
         self.parent = parent
 
-    def on_set_hook(self, tag_name: str, attr: Any, key: Any, value: Any) -> None:
+    def on_set_hook(self, tag_name: str, attr: Any, key: slice, value: List[Any]) -> None:
         pass
 
-    def on_change(self, name_prefix: str, callback=None, *, key=None, defer=False):
+    def on_change(self, name_prefix: str, callback=None, *, key=slice(0,1), defer=False):
         return self.parent.on_change(name_prefix, callback, key=key, defer=defer)
 
-    def get_val(self, name_prefix: str, key: slice | None = slice(0, 1, None)) -> Any:
+    def get_val(self, name_prefix: str, key: slice = slice(0, 1, None)) -> Any:
         data_tag = self.parent._lookup(name_prefix)
         if data_tag is None:
             return None
@@ -26,7 +26,7 @@ class Basic:
             return data_tag[key][0]
         return data_tag[key]
 
-    def set_val(self, name_prefix: str, value: Any, key: slice | None = slice(0, 1, None)):
+    def set_val(self, name_prefix: str, value: Any, key: slice = slice(0, 1, None)):
         data_tag = self.parent._lookup(name_prefix)
         if data_tag is None:
             return
