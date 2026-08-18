572e8d09cd68d1932a26b48c9d16aebd734a5f13
Stephen Chryn <SRCthird@gmail.com>
2026-08-18 16:29:18 -0400

    Create a basic example or web_api

---
+++ example/web_api/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Merck KGaA, Darmstadt, Germany and/or its affiliates.
+# All rights reserved
+
+# example/web_api/__init__.py
+from example.web_api import actions
+from example.web_api import tags
+
+__all__ = ["actions", "tags"]
